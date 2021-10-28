76d259c21f00ec97a5516e73fe82d380dfa02b56
JohnSalinas123 <salinasjohn257@gmail.com>
2021-10-27 22:30:40 -0700

    added Task 4 code

--- DriverBT.java
+++ DriverBT.java
@@ -2,9 +2,12 @@ public class DriverBT
 {
 	public static void main(String[] args)
 	{
+
+
 		System.out.println("1st Testing Example:");
 		BinaryTree<String> aTree = new BinaryTree<>();
 		createTree1(aTree);
+		
 		
 		System.out.println("(binaryTree) post-order: ");
 		aTree.postorderTraverse();
@@ -13,15 +16,15 @@ public class DriverBT
 		aTree.postorderTraverse_callBinaryNodeMethod();
 		
 		System.out.println();
-		
+
+
 		System.out.println("(BinaryTree) Height of tree is " + aTree.getHeight());
 		System.out.println("(BinaryNode) Height of tree is " + aTree.getHeight_callBinaryNodeMethod());
-		
 		System.out.println();
 		
 		System.out.println("(BinaryTree) # nodes of tree is " + aTree.getNumberOfNodes());
 		System.out.println("(BinaryNode) # nodes of tree is " + aTree.getNumberOfNodes_callBinaryNodeMethod());
-
+		
 		System.out.println("==========================================");
 		System.out.println();
 		
@@ -40,12 +43,14 @@ public class DriverBT
 		System.out.println("(BinaryTree) Height of tree is " + aTree2.getHeight());
 		System.out.println("(BinaryNode) Height of tree is " + aTree2.getHeight_callBinaryNodeMethod());
 		
+		
 		System.out.println();
 		
 		System.out.println("(BinaryTree) # nodes of tree is " + aTree2.getNumberOfNodes());
 		System.out.println("(BinaryNode) # nodes of tree is " + aTree2.getNumberOfNodes_callBinaryNodeMethod());
 		
 		System.out.println("Done.");
+
 	}  // end main
 
 	public static void createTree1(BinaryTree<String> tree)
@@ -73,10 +78,34 @@ public class DriverBT
       System.out.println();
 	} // end createTree
 	
+
 	/**-------------------------------------------------------------------- */
 	/**Task 4: Create a tree case as shown in assignment 3*/
 	public static void createTree2(BinaryTree<String> tree)
 	{ 
+
+		// Leaves
+		BinaryTree<String> bTree = new BinaryTree<>("B");
+		BinaryTree<String> fTree = new BinaryTree<>("F");
+		BinaryTree<String> gTree = new BinaryTree<>("G");	
+		BinaryTree<String> hTree = new BinaryTree<>("H");
+
+		// Subtrees:
+		BinaryTree<String> eTree = new BinaryTree<>("E", gTree, hTree);
+		BinaryTree<String> dTree = new BinaryTree<>("D", fTree, null);
+		BinaryTree<String> cTree = new BinaryTree<>("C", dTree, eTree);
+
+		tree.setTree("A", bTree, cTree);
+
+		System.out.println("\nGiven Tree:\n");
+		System.out.println("     A         ");
+		System.out.println("   /   \\      ");
+		System.out.println("  B     C      ");
+		System.out.println("       / \\    ");
+		System.out.println("      D    E   ");
+		System.out.println("     /    / \\ ");
+		System.out.println("    F    G    H");
+		System.out.println();
 
 	} // end createTree2
 
